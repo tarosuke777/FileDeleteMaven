2162edb01ab7699c57e2c03b6b44feaccb7388f3
tarosuke <felis.silvestris.catus.7777@gmail.com>
2023-10-15 20:57:22 +0900

    デバックログを削除

--- src/main/java/file/FileDelete.java
+++ src/main/java/file/FileDelete.java
@@ -3,8 +3,6 @@ package file;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,12 +16,6 @@ public class FileDelete {
       List<Path> paths = stream.collect(Collectors.toList());
 
       for (Path path : paths) {
-        System.out.println(path.getFileName());
-        System.out.println(LocalDateTime.ofInstant(ZonedDateTime.now().minusDays(7).toInstant(),
-            ZoneId.systemDefault()));
-        System.out.println(LocalDateTime.ofInstant(Files.getLastModifiedTime(path).toInstant(),
-            ZoneId.systemDefault()));
-
         if (Files.getLastModifiedTime(path).toInstant()
             .isBefore(ZonedDateTime.now().minusDays(7).toInstant())) {
           Files.deleteIfExists(path);
